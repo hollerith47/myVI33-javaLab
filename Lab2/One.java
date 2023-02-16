e4538fc9afeeafeee0baa458f98023ea13d81991
Hollerith47 <hermanhmakiese@gmail.com>
2023-02-16 19:00:33 +0300

    Lab 3 okay

--- Lab2/One.java
+++ Lab2/One.java
@@ -1,8 +1,40 @@
 package Lab2;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class One {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.print("Entrez le nombre de chaînes : ");
+        System.out.print("Введите количество строк: ");
+        int n = userInput.nextInt();
+        userInput.nextLine();
+
+        List<String> list = getList(n, userInput);
+        System.out.println(list);
+
+        String result = getShortestString(list);
+        System.out.printf("Самая короткая строка: '%s' и ее длина %d",result, result.length()).println();
+    }
+
+    public static String getShortestString(List<String> list) {
+        int minLength = Integer.MAX_VALUE;
+        String shortestString = null;
+        for (String s : list) {
+            if (s.length()  < minLength) {
+                minLength = s.length();
+                shortestString = s;
+            }
+        }
+        return shortestString;
+    }
+
+    public static List<String> getList(int n, Scanner userInput){
+        List<String> list = new ArrayList<String>();
+        for (int i = 0; i <n ; i++) {
+            System.out.printf("Введите строку %d : ", i+1);
+            String stringInput = userInput.nextLine();
+            list.add(stringInput);
+        }
+        return list;
     }
 }
--- Lab2/Two.java
+++ Lab2/Two.java
@@ -1,2 +1,39 @@
-package Lab2;public class Two {
+package Lab2;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Two {
+    public static void main(String[] args) {
+        int n = 0;
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите количество строк: ");
+            try {
+                n = userInput.nextInt();
+                userInput.nextLine();
+                break;
+            } catch (InputMismatchException fg){
+                System.out.println("Вы ввели не число. ");
+            }
+        }
+
+        String[] lines = new String[n];
+        for (int i = 0; i < n; i++){
+            System.out.printf("Введите строку %d : ", i + 1);
+            lines[i] = userInput.nextLine();
+        }
+
+        // Trie les chaînes par ordre croissant de longueur, puis par ordre croissant de valeur de longueur.
+        Arrays.sort(lines, Comparator.comparingInt(String::length).thenComparing(String::valueOf));
+
+        // Affiche les chaînes triées.
+        System.out.println("Строки в порядке убывания длины:");
+        int i = 1;
+        for (String line : lines) {
+            System.out.printf("строка %d : '%s' и ее длина %d",i, line, line.length()).println();
+            i++;
+        }
+    }
 }
